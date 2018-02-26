0c1acd07b13a5e1ab1b6e1c4a2bfe0499cb6438d
Javier Tarazaga <javier.tarazaga@valtech.se>
2018-02-26 22:30:28 +0100

    Added drawer layout instad of bottom nav bar

---
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/Main3Activity.java
@@ -0,0 +1,61 @@
+package com.fernandocejas.android10.sample.presentation.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.fernandocejas.android10.sample.presentation.R;
+import com.fernandocejas.android10.sample.presentation.view.BaseActivity;
+import com.fernandocejas.android10.sample.presentation.view.explore.ExploreFragment;
+import com.fernandocejas.android10.sample.presentation.view.feeds.FeedsFragment;
+import com.fernandocejas.android10.sample.presentation.view.home.HomeFragment;
+import com.fernandocejas.android10.sample.presentation.view.settings.SettingsFragment;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+/**
+ * Main application screen. This is the app entry point.
+ */
+public class Main3Activity extends BaseActivity {
+
+  @BindView(R.id.bottom_nav_main) BottomNavigationViewEx bottomNavigationView;
+
+  private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemReselectedListener =
+      new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+          switch (item.getItemId()) {
+            case R.id.nav_home:
+              replaceFragment(R.id.fragment_container, HomeFragment.newInstance());
+              break;
+            case R.id.nav_feeds:
+              replaceFragment(R.id.fragment_container, FeedsFragment.newInstance());
+              break;
+            case R.id.nav_explore:
+              replaceFragment(R.id.fragment_container, ExploreFragment.newInstance());
+              break;
+            case R.id.nav_settings:
+              replaceFragment(R.id.fragment_container, SettingsFragment.newInstance());
+              break;
+          }
+
+          return true;
+        }
+      };
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main3);
+    ButterKnife.bind(this);
+
+    setupView();
+  }
+
+  private void setupView() {
+    bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemReselectedListener);
+    bottomNavigationView.setCurrentItem(0);
+    bottomNavigationView.enableShiftingMode(false);
+    bottomNavigationView.enableItemShiftingMode(false);
+    bottomNavigationView.setTextVisibility(false);
+  }
+}
--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
@@ -2,60 +2,95 @@ package com.fernandocejas.android10.sample.presentation.view.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import com.fernandocejas.android10.sample.presentation.R;
 import com.fernandocejas.android10.sample.presentation.view.BaseActivity;
-import com.fernandocejas.android10.sample.presentation.view.explore.ExploreFragment;
-import com.fernandocejas.android10.sample.presentation.view.feeds.FeedsFragment;
-import com.fernandocejas.android10.sample.presentation.view.home.HomeFragment;
-import com.fernandocejas.android10.sample.presentation.view.settings.SettingsFragment;
-import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-/**
- * Main application screen. This is the app entry point.
- */
-public class MainActivity extends BaseActivity {
-
-  @BindView(R.id.bottom_nav_main) BottomNavigationViewEx bottomNavigationView;
-
-  private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemReselectedListener =
-      new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-          switch (item.getItemId()) {
-            case R.id.nav_home:
-              replaceFragment(R.id.fragment_container, HomeFragment.newInstance());
-              break;
-            case R.id.nav_feeds:
-              replaceFragment(R.id.fragment_container, FeedsFragment.newInstance());
-              break;
-            case R.id.nav_explore:
-              replaceFragment(R.id.fragment_container, ExploreFragment.newInstance());
-              break;
-            case R.id.nav_settings:
-              replaceFragment(R.id.fragment_container, SettingsFragment.newInstance());
-              break;
-          }
-
-          return true;
-        }
-      };
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
-
     setupView();
   }
 
+  @Override public void onBackPressed() {
+    DrawerLayout drawer = findViewById(R.id.drawer_layout);
+    if (drawer.isDrawerOpen(GravityCompat.START)) {
+      drawer.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main2, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @SuppressWarnings("StatementWithEmptyBody") @Override
+  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+
+    if (id == R.id.nav_camera) {
+      // Handle the camera action
+    } else if (id == R.id.nav_gallery) {
+
+    } else if (id == R.id.nav_slideshow) {
+
+    } else if (id == R.id.nav_manage) {
+
+    } else if (id == R.id.nav_share) {
+
+    } else if (id == R.id.nav_send) {
+
+    }
+
+    DrawerLayout drawer = findViewById(R.id.drawer_layout);
+    drawer.closeDrawer(GravityCompat.START);
+    return true;
+  }
+
   private void setupView() {
-    bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemReselectedListener);
-    bottomNavigationView.setCurrentItem(0);
-    bottomNavigationView.enableShiftingMode(false);
-    bottomNavigationView.enableItemShiftingMode(false);
-    bottomNavigationView.setTextVisibility(false);
+    setupToolbar();
+    setupNavigation();
+  }
+
+  private void setupToolbar() {
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    DrawerLayout drawer = findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,
+        R.string.navigation_drawer_close);
+    drawer.addDrawerListener(toggle);
+    toggle.syncState();
+  }
+
+  private void setupNavigation() {
+    NavigationView navigationView = findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
   }
 }
