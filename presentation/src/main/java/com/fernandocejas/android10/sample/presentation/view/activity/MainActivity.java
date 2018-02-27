8e6305d11b06b4e0b790d955a8a98a000031e1b0
Javier Tarazaga <javier.tarazaga@valtech.se>
2018-02-27 14:51:38 +0100

    mixed navigation view with side menu

--- presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
+++ presentation/src/main/java/com/fernandocejas/android10/sample/presentation/view/activity/MainActivity.java
@@ -2,28 +2,60 @@ package com.fernandocejas.android10.sample.presentation.view.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.fernandocejas.android10.sample.presentation.R;
 import com.fernandocejas.android10.sample.presentation.view.BaseActivity;
+import com.fernandocejas.android10.sample.presentation.view.explore.ExploreFragment;
+import com.fernandocejas.android10.sample.presentation.view.feeds.FeedsFragment;
+import com.fernandocejas.android10.sample.presentation.view.home.HomeFragment;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+  @BindView(R.id.bottom_nav_main) BottomNavigationViewEx bottomNavigationView;
+  @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+
+  private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemReselectedListener =
+      new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+          switch (item.getItemId()) {
+            case R.id.nav_home:
+              replaceFragment(R.id.fragment_container, HomeFragment.newInstance());
+              break;
+            case R.id.nav_feeds:
+              replaceFragment(R.id.fragment_container, FeedsFragment.newInstance());
+              break;
+            case R.id.nav_explore:
+              replaceFragment(R.id.fragment_container, ExploreFragment.newInstance());
+              break;
+            case R.id.nav_settings:
+              drawerLayout.openDrawer(GravityCompat.END);
+              break;
+          }
+
+          return true;
+        }
+      };
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+
     setupView();
   }
 
   @Override public void onBackPressed() {
-    DrawerLayout drawer = findViewById(R.id.drawer_layout);
-    if (drawer.isDrawerOpen(GravityCompat.START)) {
-      drawer.closeDrawer(GravityCompat.START);
+    if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+      drawerLayout.closeDrawer(GravityCompat.END);
     } else {
       super.onBackPressed();
     }
@@ -69,21 +101,22 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     }
 
     DrawerLayout drawer = findViewById(R.id.drawer_layout);
-    drawer.closeDrawer(GravityCompat.START);
+    drawer.closeDrawer(GravityCompat.END);
     return true;
   }
 
   private void setupView() {
     setupToolbar();
     setupNavigation();
+    bottomNavigationView();
   }
 
   private void setupToolbar() {
-    Toolbar toolbar = findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+    //Toolbar toolbar = findViewById(R.id.toolbar);
+    //setSupportActionBar(toolbar);
 
     DrawerLayout drawer = findViewById(R.id.drawer_layout);
-    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, null, R.string.navigation_drawer_open,
         R.string.navigation_drawer_close);
     drawer.addDrawerListener(toggle);
     toggle.syncState();
@@ -92,5 +125,13 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
   private void setupNavigation() {
     NavigationView navigationView = findViewById(R.id.nav_view);
     navigationView.setNavigationItemSelectedListener(this);
+  }
+
+  private void bottomNavigationView() {
+    bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemReselectedListener);
+    bottomNavigationView.setCurrentItem(0);
+    bottomNavigationView.enableShiftingMode(false);
+    bottomNavigationView.enableItemShiftingMode(false);
+    bottomNavigationView.setTextVisibility(false);
   }
 }
